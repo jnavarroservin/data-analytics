80e3a2c09786da08d3ad947d37cb87efd1854491
jessica navarroservin <jnavarroservin@my.yearupunited.org>
2026-04-14 18:18:54 -0500

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about the customers and their film type purchases for use by a film store
 */
+USE sakila;
+SELECT * FROM actor; -- retrieved 200 records
+
+SELECT * FROM film; -- retrieved 1000 records
