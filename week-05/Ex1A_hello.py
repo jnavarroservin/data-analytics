6d50c8293f675eefa65da02ef571a01775238f1b
jnavarroservin <jnavarroservin@my.yearupunited.org>
2026-05-05 21:47:35 -0500

    EXERCISE 1A: first python program

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,22 @@
 print ('Hello world!')
+
+message = 'Hello world!'
+print(message) 
+# 'Hello world!' printed twice because I provided
+# two different ways with the same ojective.
+
+# DISPLAYING DOLLARS AND CENTS #
+dollars = 3 
+cents = .50
+print(dollars + cents)
+# integer and float are the same data type so print as 3.5
+cents = cents + .25
+print(dollars + cents)
+# cents variable has now been updated, 
+# so now the print statement will change to reflect it.
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
+# variables are both string and print statement must include
+# a string with a space to correctly return the print statement.
