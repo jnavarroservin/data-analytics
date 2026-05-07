e659f2b896c10ba45fe4ba437e9142bc58dee85b
jnavarroservin <jnavarroservin@my.yearupunited.org>
2026-05-07 00:52:56 -0500

    EXERCISE 2A: mathscripts

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,8 @@
+# LAB 2 // PEER COLLABORATION
+# 1. How do you calculate your net worth given your assets and debts?
+assets = 1000
+debts = 300
+networth = assets - debts
+print(f'Your total assets are {assets}')
+print(f'Your total debts are {debts}')
+print(f'Your net worth is {networth}')
