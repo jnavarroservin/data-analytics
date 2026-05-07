08408404d3a8b0cc4aa51f456a375bb68e116438
jnavarroservin <jnavarroservin@my.yearupunited.org>
2026-05-07 01:01:02 -0500

    EXERCISE 2A: rectangle area

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,11 @@
+# LAB 2 // PEER COLLAB
+# #2. How do you calculate the area of a rectangle?
+
+side_a = 5
+side_b = 12
+area = side_a * side_b
+print(f'Side A is {side_a}')
+print(f'Side B is {side_b}')
+print(f'The area of the rectangle is {area}')
+
+# answer: Side A is 5. Side B is 12. The area of the rectangle is 60.
