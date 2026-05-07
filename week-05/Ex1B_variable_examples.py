9aa45b80da37478fd5885b8ce0d1e927f3e72910
jnavarroservin <jnavarroservin@my.yearupunited.org>
2026-05-06 23:47:12 -0500

    EXERCISE 1B: labs

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,29 @@
+# LAB 1
+# This is an example of how to create variables in Python.
+customer_id = 444
+customer_name = 'Lady Gaga'
+gender = 'Female'
+dob = '1986-03-28'
+dl_number = 'DL0000123'
+auto_policy_number = 'GAGA00001'
+print (customer_id, customer_name, gender, dob, dl_number, auto_policy_number)
+
+my_name = 'Jessica Navarro'
+birth_city = 'Apaseo El Alto'
+birth_state = 'Guanajuato'
+print (my_name,',', birth_city, ',', birth_state)
+
+
+#LAB 2
+# A) Reserved words in Python.
+# False	def	if	raise None	del	import	return
+# True elif in	try and	else is	^while^
+# as except	^lambda^ with assert	finally	^nonlocal^ yield
+# break	for	not class form or continue ^global^ pass
+
+#B) 5 Reseved words in Python and their function:
+# 1. if - used to make a decision based on a condition. True equals action, False equals no action.
+# 2. else - used to specify a block of code to be executed if the condition in the if statement is False.
+# 3. elif - used to specify a new condition if the previous condition is False.
+# 4. or - used to combine two conditions and returns True if either condition is True.
+# 5. and - used to combine two conditions and returns True only if both conditions are True.
