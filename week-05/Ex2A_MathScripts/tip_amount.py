1b6caad7307d017c4f9e0e9f3726037068e86896
jnavarroservin <jnavarroservin@my.yearupunited.org>
2026-05-07 21:24:48 -0500

    EXERCISE 2A: tip amount

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,14 @@
+# LAB 2 // PEER COLLAB
+#3. How do you calculate the tip amount on a restaurant bill given the tip percentage?
+
+tip_percentage = 15
+bill_amount = 53.75
+tip_amount = (tip_percentage / 100) * bill_amount
+print(f'The tip amount on a ${bill_amount} restaurant bill is: ${tip_amount:.2f}')
+    
+    
+    '''ANSWER:
+    The tip amount on a $53.75 restaurant bill is: $8.06.
+    I learned that the colon symbol helps seperate the value
+    from the format type, which allows me to limit the result
+    to only two decimal points.'''
